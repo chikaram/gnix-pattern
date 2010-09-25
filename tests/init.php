1d44b693a9c6dec00d77a8bb48f7abb7d3a04e23
chikara miyake <chikara.miyake@gmail.com>
2010-09-25 18:08:12 +0900

    init

--- tests/init.php
+++ tests/init.php
@@ -1,11 +1,9 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . '/data/App/Shared');
+mb_internal_encoding('UTF-8');
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
 
+set_include_path(get_include_path() . PATH_SEPARATOR . '/data/App/Shared');
 require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->setFallbackAutoloader(true);
-
-mb_internal_encoding('UTF-8');
-
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
